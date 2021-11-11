76bfa0658f2cf2090e5339e2d1549afa7ebe6d1f
Jacob Reichle <51340981+thesodbuster@users.noreply.github.com>
2021-11-11 17:36:00 -0500

    Login, Registration, Update Password
    Initial Commit!

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+# Database credentials for connecting to MySQL
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+
+#### IMPORTANT ####
+# YOUR MYSQL PASSWORD HERE
+define('DB_PASSWORD', '');
+define('DB_NAME', 'users');
+
+# Attempt to connect to the database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+# Check the connection
+if ($link === false) {
+    die("ERROR: Cannot connect to DB. " . mysqli_connect_error());
+}
+?>

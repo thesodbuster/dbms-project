64a30b798b6a659dcf0a16b40c35dba948fc1ad5
Jacob Reichle <51340981+thesodbuster@users.noreply.github.com>
2021-11-21 20:37:11 -0500

    Incomplete updates to singlemail.php

--- singlemail.php
+++ singlemail.php
@@ -8,22 +8,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     exit;
 }
 
-$msg = $msg_err = $rec_email = $rec_email_err = "";
+# Declare variables as empty strings
+$msg = $msg_err = "";
+$rec_email = $rec_email_err = "";
+$subject = $subject_err = "";
 $user_email = $_SESSION["email"];   # Logged in user's email
 
-# Check to see if message field is left blank
+# Do this once the submit button is pressed
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    # Throw error if message box is emtpy
     if(empty(trim($_POST["msg"]))){
         $msg_err = "Please enter content for the message"
     } else {
         $msg = trim($_POST["msg"]);
     }
 
+    # Throw error if email field is empty
     if(empty(trim($_POST["rec_email"]))){
         $rec_email_err = "Please enter an email to send to";
     } else {
         $rec_email = trim($_POST["rec_email"]);
     }
+
+    # Send the email now that the variables are stored
+    #mail($toemail, $subject, $message, $headers); ### REMINDER
+    mail($rec_email, )
 }
 
 
@@ -49,12 +59,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <p>Announcement to all Professors</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
-                <label>Email</label>
+                <label>To:</label>
                 <input type="text" name="rec_email" class="form-control <?php echo (!empty($date_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $rec_email; ?>">
                 <span class="invalid-feedback"><?php echo $rec_email_err; ?></span>
             </div>
             <div class="form-group">
-                <label>Message</label>
+                <label>Subject:</label>
+                <input type="text" name="subject" class="form-control <?php echo (!empty($subject_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $subject; ?>">
+                <span class="invalid-feedback"><?php echo $subject_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Message:</label>
                 <input type="text" name="msg" class="form-control <?php echo (!empty($date_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $msg; ?>">
                 <span class="invalid-feedback"><?php echo $msg_err; ?></span>
             </div>

48fb0aa63d682898ad09322c713ff38c265a99b7
Jacob Reichle <51340981+thesodbuster@users.noreply.github.com>
2021-11-22 14:39:03 -0500

    WORKING
    less go

--- forgotpassword.php
+++ forgotpassword.php
@@ -3,7 +3,21 @@
 require_once "config.php";
 
 # Function to generate a random 10 character password
+function randName() {
+    $characters = '0123456789qwertyuioplkjhgfdsazxcvbnm';
+    $str = '';  # Start with empty string
 
+    # Randomly select 10 characters
+    for($i = 0 ; $i < 10 ; $i++){
+        # Generate a random index within strlen of character array
+        $ind = rand(0, strlen($characters) - 1);
+        
+        # Concat character onto string
+        $str .= $characters[$ind];
+    }
+
+    return $str;
+}
 
 # Declare variables
 $email = $email_err = $confirm_email_err = $confirm_email = "";
@@ -28,7 +42,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     # Now generate a random string as a temporary password
-    $newpassword = 
+    $newpassword = randName();
+
+    # Execute an update query to change the password to the randomly generated one
+    $sql = "UPDATE user SET password = ? WHERE email = ?";
+
+    if($stmt = mysqli_prepare($link, $sql)){
+        mysqli_stmt_bind_param($stmt, "ss", $param_new_password, $param_email);
+
+        # Set parameters
+        $param_new_password = password_hash($newpassword, PASSWORD_DEFAULT);
+        $param_email = $email;
+
+        # Attempt to execute the statement
+        if(mysqli_stmt_execute($stmt)){
+            # Password update successfully
+            echo "Password updated successfully";
+
+            # Send an email to the user with their password
+            $msg = "Your password on the book order website has been reset to: " . $newpassword;
+            $subject = "Book Website Password Reset";
+            $headers = "From: databaseproject24 @ gmail.com";
+
+            # Send the email message
+            mail($email, $subject, $msg, $headers);
+
+            # Now redirect to the login page
+            header("location: login.php");
+        } else {
+            echo "Something went wrong creating a temporary password";
+        }
+        mysqli_stmt_close($stmt);
+    }
+
 }
 
 
@@ -60,7 +106,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <label>Confirm Email</label>
-                <input type="password" name="confirm_email" class="form-control <?php echo (!empty($confirm_email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_email; ?>">
+                <input type="text" name="confirm_email" class="form-control <?php echo (!empty($confirm_email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_email; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_email_err; ?></span>
             </div>
             <div class="form-group">

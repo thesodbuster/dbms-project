72ab167c618fff21374beb7736da1c101158fea3
Jacob Reichle <51340981+thesodbuster@users.noreply.github.com>
2021-11-24 12:28:06 -0500

    Book Order page

--- welcome.php
+++ welcome.php
@@ -35,6 +35,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+        <a href="bookorder.php" class="btn btn-primary">Submit a Book Order</a>
     </p>
 </body>
 </html>
